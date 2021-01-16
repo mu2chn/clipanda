c734e624253567ff7d0e0d31b150e2eb8cfe9657
face0u0 <me@face0u0.net>
2021-01-17 02:05:18 +0900

    widget

--- widget.py
+++ widget.py
@@ -1,6 +1,7 @@
 import clipanda
 import sys, tkinter
 from tkinter import filedialog
+from tkinter import ttk
 
 DEFAULT_HEIGHT = 40
 
@@ -33,12 +34,34 @@ class Button:
     def onLeftClick(self, func):
         self.__button.bind('<Button-1>', func=lambda arg: func())
 
-def dbgShow(arg):
-    print(arg)
-    pass
+class DropDown:
+    def __init__(self, root, list, x=0, y=0):
+        frame = ttk.Frame(root, padding=10)
+        frame.place(x=x, y=y)
 
-def selectFolder():
-    return tkinter.filedialog.askdirectory(initialdir="~/")
+        v = tkinter.StringVar()
+        self.__cb = ttk.Combobox(
+            frame,
+            textvariable=v, 
+            values=list, width=10)
+        self.__cb.set(list[0])
+        self.__cb.bind(
+            '<<ComboboxSelected>>', 
+            lambda e: print('v=%s' % v.get()))
+        self.__cb.place(x=10, y=300)
+        self.__cb.grid(row=0, column=0)
+
+    def getVal(self):
+        return self.__cb.get()
+
+    def destroy(self):
+        self.__cb.destroy()
+
+pc = None
+
+def init_pc(username, password):
+    cookie = clipanda.PandaClient.createSession(username, password)
+    pc = clipanda.PandaClient(cookie)
 
 def render():
     root = tkinter.Tk()
@@ -51,12 +74,15 @@ def render():
     username = EntryBox(label="username", x=loginX, y=loginY)
     password = EntryBox(label="password", x=loginX, y=loginY+DEFAULT_HEIGHT, hidechar=True)
 
-    button = Button(text="ログイン", x=loginX+330, y=loginY+DEFAULT_HEIGHT-8)
-    button.onLeftClick(lambda : print(username.getText(), password.getText()))
+    button = Button(text="login", x=loginX+330, y=loginY+DEFAULT_HEIGHT-8)
+    button.onLeftClick(lambda : init_pc(username.getText(), password.getText()))
 
     refDir = EntryBox(label="path", x=10, y=160, entrywidth=40, textwidth=40)
-    button = Button(text="参照", x=380, y=155)
+    button = Button(text="reference", x=380, y=155)
     button.onLeftClick(lambda : refDir.setText(filedialog.askdirectory(initialdir="~/Documents")))
+
+    DropDown(root, ["A", "B", "C"], x=10, y=250)
+
     root.mainloop()
 
 
